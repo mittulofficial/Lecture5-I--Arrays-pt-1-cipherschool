0928fbf300a5e75c5aa284d72c4089c54c25c3d8
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-08 20:31:51 +0530

    Create withoutarray.cpp
    this is example without using any array

---
+++ withoutarray.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lecture5 I- Arrays pt 1
+//
+//  Created by MITTUL KUMAR on 8/0/23.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    cout<<"give 5 integers as input"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"in nreverse order"<<e<<d<<c<<b<<a;
+    return 0;
+    
+}
